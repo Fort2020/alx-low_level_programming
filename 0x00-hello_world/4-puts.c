236b63073037e42b666690b9188842afc888ef86
fort2020 <frankfortdata@gmail.com>
2022-10-15 08:20:40 -0700

    Task 4. My first C programming writing in Alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print exactly this line "Programming is like building a multilingual puzzle using puts
+ *
+ * Return: 0 if successfully printed
+ */
+int (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
