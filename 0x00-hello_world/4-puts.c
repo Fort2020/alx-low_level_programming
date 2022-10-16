e02a1502db318a753e6ea949781face4b43044f9
fort2020 <frankfortdata@gmail.com>
2022-10-15 22:48:44 -0700

    Task 4. done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main - print exactly this line "Programming is like building a multilingual puzzle using puts
- *
- * Return: 0 if successfully printed
+/**
+ * main - Entry point
+ * Return: 0 success
  */
-int (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+	return (0);
 }
