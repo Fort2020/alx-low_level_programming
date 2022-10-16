08d167a2056e5e9462b0a182a4a798342bb2f154
fort2020 <frankfortdata@gmail.com>
2022-10-16 09:33:33 -0700

    Task 5. done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print C program with printf
+ * Return: 0 as always
+*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
