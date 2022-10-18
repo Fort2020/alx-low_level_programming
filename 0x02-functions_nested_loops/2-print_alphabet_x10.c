975d4dfe56e211371631ce1a7749003310cf6f75
fort2020 <frankfortdata@gmail.com>
2022-10-18 01:46:37 -0700

    Task 2. done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints 10-x alphabet in lowercase, each in new line
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int count = 0;
+	chat letter;
+
+	while (count++ <= 9}
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
