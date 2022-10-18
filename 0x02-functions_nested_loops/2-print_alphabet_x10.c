bda8822468e114ff29babf2678b54e33ec6ecd3b
fort2020 <frankfortdata@gmail.com>
2022-10-18 11:46:31 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
 
 {
 	int count = 0;
-	chat letter;
+	char letter;
 
 	while (count++ <= 9)
 	{
