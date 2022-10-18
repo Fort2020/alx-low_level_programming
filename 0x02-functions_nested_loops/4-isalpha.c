8979e99571d2ab298916d181dbdb33356029a25d
fort2020 <frankfortdata@gmail.com>
2022-10-18 13:09:28 -0700

    Task 4. done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks if a char is alphabetic
+ * _isalpha - Checks if a character is alphabetic
  * @c: The character to be checked
- *
  * Return: 1 if character is letter lowercase, 0 otherwise
  */
 int _isalpha(int c)
