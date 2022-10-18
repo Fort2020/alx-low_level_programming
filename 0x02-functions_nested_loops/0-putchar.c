9430230c5f097e01da7e0ae7b69dd200e8410da2
fort2020 <frankfortdata@gmail.com>
2022-10-18 09:38:28 -0700

    Task 0.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints '_putchar' in each new line
- * Return: always 0
+ * main -print _putchar
+ * written by Fortman
+ * Return: 0
  */
+
 int main(void)
 {
-	_putchar ('-');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
-	_putchar ('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
