94760051e63866c44e355e854c707305258faa71
fort2020 <frankfortdata@gmail.com>
2022-10-18 14:47:12 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - Prints the sign of a number.
  * @n: the number of which the sign will be checked.
  * Return 1 and prints + if n is greater than zero.
- *	0 and prints 0 if n is zero.
- *	-1 and prints - if n is less than zero.
+ * 0 and prints 0 if n is zero.
+ * -1 and prints - if n is less than zero.
  */
 int print_sign(int n)
 {
@@ -14,14 +14,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
 		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
