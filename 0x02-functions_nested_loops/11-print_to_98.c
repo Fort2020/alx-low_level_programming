69e280d41384d054e31df92d62c57f5e79c6c96d
fort2020 <frankfortdata@gmail.com>
2022-10-19 06:11:16 -0700

    Task 11. done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98, followed by a new line.
+ * it must be separated by a comma, followed by a space.
+ * @n: the number begin counting.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
