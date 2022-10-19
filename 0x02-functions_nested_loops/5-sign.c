8fc3ae44db21e8e14136b2b542c6383e7cf8ba41
fort2020 <frankfortdata@gmail.com>
2022-10-18 22:28:53 -0700

    Task 5. done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the number of which the sign will be checked
- * Return 1 if the number is greateer than zero
+ * print_sign - Prints the sign of a number.
+ * @n: the number of which the sign will be printed.
+ * Return: 1 if the number is greateer than zero,
  * 0 if the number is zero,
- * -1 is the number is less than zero
+ * -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(+);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -24,5 +24,4 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('\n');
 }
