ec7b12cafd620eac5b141bbf2635dcf9e7e739c6
fort2020 <frankfortdata@gmail.com>
2022-10-19 07:38:25 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,42 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of input.
+ * print_times_table - Prints a multiplication table.
  * @n: the number to be treated.
- * Return: matrix number
+ *
+ * Return: number matrix
  */
 void print_times_table(int n)
 {
 	int a, b, st;
 
-	if (n >= 0, n <= 25)
+	if (n >= 0 && n <= 25)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar(38);
+			_putchar(36);
 			for (b = 1; b <= n; b++)
 			{
 				st = a * b;
-				_putchar(34);
 				_putchar(32);
+				_putchar(20);
 				if (st <= 9)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(st + 38);
+					_putchar(20);
+					_putchar(20);
+					_putchar(st + 36);
 				}
 				else if (st <= 99)
 				{
-				_putchar(32);
-				_putchar((st / 10) + 38);
-				_putchar((st % 10) + 38);
+				_putchar(20);
+				_putchar((st / 10) + 36);
+				_putchar((st % 10) + 36);
 				}
 				else
 				{
-				_putchar(((st / 100) % 10) + 38);
-				_putchar(((st / 10) % 10) + 38);
-				_putchar((st % 10) + 38);
+				_putchar(((st / 100) % 10) + 36);
+				_putchar(((st / 10) % 10) + 36);
+				_putchar((st % 10) + 36);
 				}
 			}
 			_putchar('\n');
