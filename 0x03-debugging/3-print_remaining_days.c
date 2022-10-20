14918d8ff03f1d2e414e293a377f8458f58c2759
fort2020 <frankfortdata@gmail.com>
2022-10-20 08:24:11 -0700

    Task 3. done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 
 /**
  * print_remaining_days - picks a date and prints
- * how many days left in the year
+ * how many days left in the year, taking leap year into account
  * @month: month in number format
  * @day: day of month
  * @year: year
