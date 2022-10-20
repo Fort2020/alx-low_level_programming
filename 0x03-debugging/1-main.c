2f98f43fa4afa98fc985baa0d377072dcf21940c
fort2020 <frankfortdata@gmail.com>
2022-10-20 04:10:25 -0700

    done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - code that is causing output to go into an infinite loop.
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ *
+*	putchar(1);
+ * }
+ */
+printf("Infinite loop avioded! \\o/\n");
+
+return (0);
+}
