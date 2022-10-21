1b4f1ed72e75ccc1d173efeb6583f881e8071ff1
fort2020 <frankfortdata@gmail.com>
2022-10-21 14:41:24 -0700

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the largest prime number 612852475143,
@@ -8,21 +9,21 @@
  */
 int main(void)
 {
-	long prime = 612852475143, div;
+long prime = 612852475143, div;
 
-	while (div < (prime / 2))
-	{
-		if ((prime % 2) == 0)
-		{
-			prime /= 2;
-			continue;
-		}
-		for (div = 3; div < (prime / 2); div += 2)
-		{
-			if ((prime % div) == 0)
-				prime /= div;
-		}
-	}
-	printf("%ld\", prime);
-	return (0);
+while (div < (prime / 2))
+{
+if ((prime % 2) == 0)
+{
+prime /= 2;
+continue;
+}
+for (div = 3; div < (prime / 2); div += 2)
+{
+if ((prime % div) == 0)
+prime /= div;
+}
+}
+printf("%ld\", prime);
+return (0);
 }
