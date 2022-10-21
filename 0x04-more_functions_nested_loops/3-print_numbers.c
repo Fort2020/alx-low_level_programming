e0e8aaa3301675772346eb16ff893ecb191fbb85
fort2020 <frankfortdata@gmail.com>
2022-10-21 02:43:49 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints 0123456789\n
+ * print_numbers - function that prints the numbers 0-9
  *
- * Return: void
+ * Return: 0-9 followed by new line
  */
 void print_numbers(void)
 {
-	char c;
+char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+for (c = 0; c <= 9; c++)
+
+_putchar(c + '0');
+
+_putchar('\n');
 
 }
