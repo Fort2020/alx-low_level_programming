d7bd7fa3c6b927498c7634c1a383d677b969f862
fort2020 <frankfortdata@gmail.com>
2022-10-24 06:06:38 -0700

    Task 5. done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 
 /**
- * rev_string - a function that reverses a string
- * @s: string to be reversed
+ * rev_string - reverse a string
+ * @s: string to reverse
  */
 void rev_string(char *s)
 {
-	int a = 0, b, c
+	int ini = 0, fb = 0;
 	char d;
 
-	while (s[a] != '\0')
+	while (s[fb] != 0)
+		fb++;
+	fb--;
+
+	while (ini < fb)
 	{
-		a++;
-	}
-	c = a - 1;
-	for (b = 0; c >= 0 && b < c; c--, b++)
-	{
-		d = s[b];
-		s[b] = s[c];
-		s[c] = d;
+		d = s[ini];
+		s[ini] = s[fb];
+		s[fb] = d;
+		ini++;
+		fb--;
 	}
 }
