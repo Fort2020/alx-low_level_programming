90640c4ab14ccccca52ccdb9ddae45a591c8fc44
fort2020 <frankfortdata@gmail.com>
2022-10-24 05:11:21 -0700

    Task 4. done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,9 +3,7 @@
 
 /**
  * print_rev - print a string in reverse, followed by a new line
- * @s: string to be printed
- *
- * Return: void
+ * @s: parameter to change
  */
 void print_rev(char *s)
 {
