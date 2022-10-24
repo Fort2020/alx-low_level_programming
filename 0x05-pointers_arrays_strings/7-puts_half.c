ce2b21e0e6bc6fff801a08e8981fe5ce0f07c16d
fort2020 <frankfortdata@gmail.com>
2022-10-24 09:10:33 -0700

    Task 7. done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,25 +7,19 @@
  */
 void puts_half(char *str)
 {
-	int i, len, n;
+	int index = 0, len = 0, n;
 
-	for (len = 0; str[len] != '\0'; len++)
-	{
-	}
+	while (str[index]
+		len++;
 
 	if ((len % 2) == 0)
 		n = len / 2;
 	else
 
 		n = (len - 1) / 2;
-		n = (len + 1) / 2;
 
-	for (i = n; i < len; i++)
-	{
-		if (str[i] == ' ')
-			continue;
-		putchar(str[i]);
-	}
+	for (index = n; index < len; index++)
+		putchar(str[index]);
 
 	putchar('\n');
 }
