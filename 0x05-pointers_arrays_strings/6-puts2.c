1aa45998a029793d8d241899058da209d00667f4
fort2020 <frankfortdata@gmail.com>
2022-10-24 07:28:19 -0700

    Task 6. done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 
 /**
- * puts2 - this function print only even numbers 
- * @str: main character
- * Return: always 0
+ * puts2 - prints one character of two out of a string
+ * @str: the main characters
  */
 void puts2(char *str)
 {
-	int a = 0;
-
-	while (str[a] != 0)
+	while (*str)
 	{
-		if ((str[a] % 2) == 0)
-		if ((a % 2) == 0)
-			putchar(str[a]);
-		a++;
+		putchar(*str);
+		str += 2;
 	}
+
 	putchar('\n');
 }
