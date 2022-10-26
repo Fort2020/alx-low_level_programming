1c6574bd7d6eac1f1cceab8319826906b32cbb0a
fort2020 <frankfortdata@gmail.com>
2022-10-26 14:12:36 -0700

    Task 4. done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * reverse_array - reverses an array
- * of an array of integers
- * @a: array of integers
+ * reverse_array - reverses an array of integers
+ * @a: array
  * @n: number of elements of the array
  *
  * Return: rev
