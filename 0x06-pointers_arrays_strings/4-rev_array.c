3c049e81c19cdf480f1e20c276b1e3e81e20ed2a
fort2020 <frankfortdata@gmail.com>
2022-10-26 14:17:58 -0700

    dont

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,13 +3,12 @@
 /**
  * reverse_array - reverses an array of integers
  * @a: array
- * @n: number of elements of the array
- *
- * Return: rev
+ * @n: integer
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int i, 
+	int i;
 	int j;
 
 	for (i = 0; i < n; i++)
