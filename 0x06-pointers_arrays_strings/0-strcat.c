4156f3901db1029aee797fdbc8cd2854996414f0
fort2020 <frankfortdata@gmail.com>
2022-10-26 02:25:09 -0700

    Task 0. done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatrnates two strings
+ * depends the dest and src
+ * @dest: a pointer to the string
+ * @src: the source to be appended to dest
+ *
+ * Return: the pointer to the dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, dest_l = 0;
+
+	while (dest[i++])
+		dest_l++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_l++] = src[i];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My readme in 0x06. C
