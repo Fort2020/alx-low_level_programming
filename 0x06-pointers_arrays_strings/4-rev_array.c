9ffa35835f5b2425cf099ea8498d6ce071046913
fort2020 <frankfortdata@gmail.com>
2022-10-26 14:03:22 -0700

    Task 4. done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * reverse_array - a funtion that reverses the content
+ * reverse_array - reverses an array
  * of an array of integers
- * @a: array of integers to be reversed
+ * @a: array of integers
  * @n: number of elements of the array
  *
  * Return: rev
  */
 void reverse_array(int *a, int n)
 {
-	int s, t;
+	int c, d;
 
-	for (s = 0; s < n; s++)
+	for (c = 0; c < n; c++)
 	{
 		n--;
-		t = a[s];
-		a[s] = a[n];
-		a[n] = s;
+		d = a[c];
+		a[c] = a[n];
+		a[n] = c;
 	}
 }
