a487309d25d339c7092b538529b7d35be7d213c6
fort2020 <frankfortdata@gmail.com>
2022-10-26 13:40:24 -0700

    Task 3. done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * _strcmp - a function that compare two string
- * @s1: string
- * @s2: strmp
+ * _strcmp - a function that compare two strings
+ * @s1: string 1
+ * @s2: string 2
  *
- * Return: int
+ * Return: the difference in s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++
-	}
+	int a;
 
-	if (*s1 == *s2)
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
 	{
-		return (0);
+		if (s1[a] != s2[a])
+		{
+		return (s1[a] - s2[a]);
+		}
+	a++;
 	}
-
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (0);
 }
