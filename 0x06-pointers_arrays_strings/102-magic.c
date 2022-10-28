3ab10e3e2089c17c71eeeee3725b1af25019943c
fort2020 <frankfortdata@gmail.com>
2022-10-28 09:45:43 -0700

    Task 10. done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of here...
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
