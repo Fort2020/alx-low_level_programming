5da5a75072e8d4c1d7feb717fa3d031c7765d1f0
fort2020 <frankfortdata@gmail.com>
2022-10-31 09:12:10 -0700

    Task 0. done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
+ * _memset - fills memory with a constant byte
+ * @n: the number of bytes to be filled
+ * @s: a pointer to the memory area to be filled
+ * @b: the charcter to fill the memory area with
+ *
+ * Return: a pointer to the filled memory area @s
  */
 char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int i;
+	char *memory = s, value = b;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+		memory[i] = value;
+
+	return (memory);
 }
