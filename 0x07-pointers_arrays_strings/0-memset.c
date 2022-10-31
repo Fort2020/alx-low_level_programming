5fd987a38837f0b9286127b3e6531d70431b9349
fort2020 <frankfortdata@gmail.com>
2022-10-31 16:37:28 -0700

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* _memset - fills the first n bytes of the memory area
-* pointed to by @s with constant byte @b
-* @n: bytes of the memory pointed by @s
-* @s: witht the constant byte @b
+* _memset - function fill the first n bytes of the memory
+* to by @s with the constant byte @b
+* @n: bytes of the memory area pointed to by @s
+* @s: with the constant byte @b
 * @b: memory area pointer
 *
 * Return: a pointer to the memory area @s
 */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
