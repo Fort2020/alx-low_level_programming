0d5d97762498d4b6171082da7ed7dc60d868dbf2
fort2020 <frankfortdata@gmail.com>
2022-11-01 00:52:45 -0700

    Task 5. done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,47 @@
 #include "main.h"
 
 /**
- * _strstr - locates a substring
- * @haystack: the string to search
- * @needle: the first occurence of the substring
+ * _strstr - locates a substring within a string
+ * @haystack: pointer that contains the address of the string to be evaluated
+ * @needle: pointer that contains the string to evaluate @haystack
  *
- * Return: a pointer beginning of substring
- * @NULL if not found
+ * Return: address of the located string in @haystack
+ * if no substring is found then return NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned  int s = 0, t = 0;
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
 
-	while (haystack[s])
+	if (*needle == 0)
 	{
-		while (needle[t] && (haystack[s] == needle[0]))
+		return (haystack);
+	}
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		a = i;
+		while (needle[j] != 0)
 		{
-
-		if (haystack[s + t] == needle[t])
-			t++;
-		else
-			break;
+			if (needle[j] == haystack[a])
+			{
+				compare = 1;
+			}
+			else
+			{
+				compare = 0;
+				break;
+			}
+			j++;
+			a++;
 		}
-		if  (needle[t])
+		if (compare == 1)
 		{
-			s++;
-			t = 0;
+			return (haystack + i);
 		}
-		else
-			return (haystack + s);
+		i++;
 	}
 	return (0);
-}
-		}
-		haystack++;
-	}
-	return ('\0');
 }
