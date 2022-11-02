df4ed7c2f7ff2c9c1d3c19e1a48e0a2d5e3e5084
fort2020 <frankfortdata@gmail.com>
2022-11-02 15:43:50 -0700

    Task 0. done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include ."main.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string followed by a new line
@@ -9,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(s *)
+		putchar(s *)
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+		putchar('\n');
 }
