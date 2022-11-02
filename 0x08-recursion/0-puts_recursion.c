027d6b59615ef1d6c444fbac0e2013936211b54a
fort2020 <frankfortdata@gmail.com>
2022-11-02 16:20:04 -0700

    Task 0. done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == 0)
 
 	{
+		_putchar(10);
+	}
+	else
+	}
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
