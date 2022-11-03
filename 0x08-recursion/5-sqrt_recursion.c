04833be643cfc9dbd0961a1fc0d3aa546222c0c5
fort2020 <frankfortdata@gmail.com>
2022-11-03 13:28:04 -0700

    Task 5. done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 int squareroot(int n, int i);
 
 /**
- * _sqrt_recursion - no loop, 1. 2nd function  - checks for square
+ * _sqrt_recursion - no loop, 1. 2nd function  - checks for perfect square
  * @n: input
- * Return: 0
+ * Return: 0 Always
  */
 int _sqrt_recursion(int n)
 {
@@ -20,12 +19,12 @@ int _sqrt_recursion(int n)
  * @i: counter
  * Return: if square root
  */
-int sqareroot(int n, int i)
+int squareroot(int n, int i)
 {
 	if (i < 1)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else
-		return (sqareroot(n, i - 1));
+		return (squareroot(n, i - 1));
 }
