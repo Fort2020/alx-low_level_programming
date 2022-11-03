051da2b10ec66bc90a8748abd653a05570a1656e
fort2020 <frankfortdata@gmail.com>
2022-11-03 05:33:42 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
- * @s: string
+ * _puts_recursion - prints a string
+ * @s: pointer block of memory to fill
  * Return: void
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar("\n");
+		_putchar('\n');
 		return;
 	}
 		_putchar(*s);
