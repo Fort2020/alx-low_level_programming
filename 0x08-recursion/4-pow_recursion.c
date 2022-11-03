eb355379b81684bc947576810f221b7b9e00797b
fort2020 <frankfortdata@gmail.com>
2022-11-03 10:42:08 -0700

    Task 4. done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: input number
- * Return: factorial of the number
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x: one input
+ * @y: two input
+ * Return: Always 0
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
-	else if (n == 0)
+	else if (y == 0)
 		return (1);
 	else
-		return (n * factorial(n - 1));
+		return (n * _pow_recursion(x, y - 1));
 }
