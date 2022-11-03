121d602c751308d6b033f6ad59d922bd4ef167d0
fort2020 <frankfortdata@gmail.com>
2022-11-03 13:17:03 -0700

    Task 4. done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
  * _pow_recursion - return the value of x raised to the power of y
@@ -10,8 +10,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 0)
 		return (1);
 	else
-		return (n * _pow_recursion(x, y - 1));
+		return (x  * _pow_recursion(x, y - 1));
 }
