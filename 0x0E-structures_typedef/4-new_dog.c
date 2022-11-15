f5b9c9df78bd6ce2c6936e788ce8bb94c536e157
fort2020 <frankfortdata@gmail.com>
2022-11-15 12:28:39 -0800

    Task 4. done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
-	if (dog == (NULL)
+	if (dog == (NULL))
 		return (NULL);
 	for (sl = 0; name[sl]; sl++)
 		;
