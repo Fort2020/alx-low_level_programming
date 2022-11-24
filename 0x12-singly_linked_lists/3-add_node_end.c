cb4a5d5fba504f37a18a8a26fc0a0030c9587c24
fort2020 <frankfortdata@gmail.com>
2022-11-24 15:00:26 -0800

    Task 3. done

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function that adds new node
+ * at the end of a list_t list
+ * @head: double pointer to a linked list
+ * @str: string to add the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
