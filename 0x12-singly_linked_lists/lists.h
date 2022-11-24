d38c87aeb2ab6333a1ff66ad6ab2f58f30a30c2f
fort2020 <frankfortdata@gmail.com>
2022-11-24 04:55:06 -0800

    Task 0. done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,13 +11,13 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description singly linked list node structure
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct lists_s *next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
