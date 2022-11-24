ffec042b6bb4ea63679631a7737c5de8868aec50
fort2020 <frankfortdata@gmail.com>
2022-11-24 15:50:55 -0800

    Task 5. done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 /**
+ * thetortoiseAndtheHare - apply the constructor
+ * attribute to thetortoiseAndtheHare()
+ * so that it will be executed before main()
+ */
+void thetortoiseAndtheHare(void) __attribute__ ((constructor));
+
+/**
  * thetortoiseAndtheHare - implementation of thetortoiseAndtheHare
- *
- * Return: always 0
  */
 void thetortoiseAndtheHare(void)
 {
