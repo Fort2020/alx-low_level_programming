2bc0ae61fac11a0ac836f1e048d7f8068012acdc
fort2020 <frankfortdata@gmail.com>
2022-11-24 15:32:19 -0800

    Task 5. done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * tortoiseAndHare - Entry point
- *
- * it should be executed before main()
+ * thetortoiseAndtheHare - implementation of thetortoiseAndtheHare
  */
-void tortoiseAndHare(void)
+void thetortoiseAndtheHare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
