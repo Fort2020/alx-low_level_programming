01998d6337543aa03718255dc00814cfac0b40e5
fort2020 <frankfortdata@gmail.com>
2022-11-28 12:43:40 -0800

    Task 8. done

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,3 +3,19 @@
 /**
  * sum_listint - function that returns the sum of all
  * the data (n) of a list linked
+ * @head: head of the list
+ *
+ * Return: if the list is empty return 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
