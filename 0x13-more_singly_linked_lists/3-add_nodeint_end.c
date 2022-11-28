5ad126ef85866f4b1990e9710664bdb43fb0e740
fort2020 <frankfortdata@gmail.com>
2022-11-28 12:17:47 -0800

    Task 3. done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,7 @@
  * @head - head of a list
  * @n: the n element
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
