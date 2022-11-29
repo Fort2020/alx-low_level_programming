3410451848f0860d96fe359866ceb15d12442282
fort2020 <frankfortdata@gmail.com>
2022-11-29 04:52:50 -0800

    Task 9. done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a new node
- * at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of the list
  * @idx: index of the list where the new node is added
  * @n: integer element
@@ -11,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int s;
+	unsigned int i;
 	listint_t *new;
 	listint_t *h;
 
@@ -19,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (s = 0; s < idx - 1 && h != NULL; s++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
@@ -37,5 +36,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		*head = new;
 	}
+	else
+	{
+		new->next = *head;
+		h->next = new;
+	}
+
 	return (new);
 }
