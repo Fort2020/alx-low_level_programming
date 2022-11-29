26360d02566a911a8fa8b49d44d8b652ad6cb047
fort2020 <frankfortdata@gmail.com>
2022-11-29 07:26:04 -0800

    Task 11. done

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -16,11 +16,12 @@ listint_t *reverse_listint(listint_t **head)
 
 	while (*head != NULL)
 	{
-		n - (*head)->next;
+		n = (*head)->next;
 		(*head)->next = p;
 		p = *head;
 		*head = n;
 	}
+
 	*head = p;
 	return (*head);
 }
