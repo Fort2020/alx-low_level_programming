1dace247ff38fc1d08f4a4fa6c06a67173c1b061
fort2020 <frankfortdata@gmail.com>
2023-05-10 13:58:38 -0700

    Updating All Tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,24 +9,24 @@
  * @size: size of the array
  * @value: value to search
  *
- * Return: return -1
+ * Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t n;
+	size_t i;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
 
-	for (n = 0; n < size; n++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%li] = [%i]\n", n, array[n]);
-		if (array[n] == value)
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
 		{
-			return (n);
+			return (i);
 		}
 	}
 	return (-1);
