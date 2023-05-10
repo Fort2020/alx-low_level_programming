975ae1855518495401f21f51f3ccdbd9544faaa6
fort2020 <frankfortdata@gmail.com>
2023-05-10 16:06:26 -0700

    Fixing Task 7.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,21 @@
 #include "search_algos.h"
+#include <math.h>
+
+/* Compile  math.h with gcc required here */
+
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
 
 /**
  * jump_search - a function that searches for a value in a
